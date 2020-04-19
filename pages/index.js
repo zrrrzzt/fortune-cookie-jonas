ed22a6da4e6ee83959253438747846af80fa1b03
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-04-19 08:42:50 +0200

    Updates to more modern ways (patch)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 const cookies = require('fortune-cookie')
 const shuffle = require('knuth-shuffle').knuthShuffle
 
@@ -7,72 +7,63 @@ function getCookie () {
   return cookie[0]
 }
 
-const Index = class extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      fortune: 'My wisdom is limitless. Sharing is caring.'
-    }
-    this.tellFortune = this.tellFortune.bind(this)
+const Index = () => {
+  const [fortune, setFortune] = useState('My wisdom is limitless. Sharing is caring.')
+
+  const tellFortune = () => {
+    setFortune(getCookie())
   }
 
-  tellFortune () {
-    const cookie = getCookie()
-    this.setState({ fortune: cookie })
-  }
-
-  render () {
-    return (
-      <div className={'wrapper'}>
-        <h1>The wisdom of Maccyber</h1>
-        <img src={'/maccyber.png'} />
-        <div className='fortune-box'>
-          {this.state.fortune}
-        </div>
-        <p>
-          <button onClick={this.tellFortune}>Give me more wisdom</button>
-        </p>
-        <style jsx global>
-          {`
-            body {
-              background-color: pink;
-              padding: 20px;
-            }
-            img {
-              border-radius: 25px;
-            }
-            button {
-              font-weight: 500;
-              width: 400px;
-              height: 60px;
-              font-size: 30px
-              border-radius: 25px;
-              border: 1px solid black;
-              cursor: pointer;
-              display: inline-block;
-              text-decoration: none;
-              background-color: lime;
-              outline: 0;
-            }
-            button:focus {
-              outline:0;
-            }
-            button:active {
-              outline: 0;
-              background-color: yellow;
-            }
-            .fortune-box {
-              font-size: 36px;
-              padding: 15px;
-            }
-            .wrapper {
-              text-align: center;
-            }
-          `}
-        </style>
+  return (
+    <div className={'wrapper'}>
+      <h1>The wisdom of Maccyber</h1>
+      <img src={'/maccyber.png'} />
+      <div className='fortune-box'>
+        { fortune }
       </div>
-    )
-  }
+      <p>
+        <button onClick={tellFortune}>Give me more wisdom</button>
+      </p>
+      <style jsx global>
+        {`
+          body {
+            background-color: pink;
+            padding: 20px;
+          }
+          img {
+            border-radius: 25px;
+          }
+          button {
+            font-weight: 500;
+            width: 400px;
+            height: 60px;
+            font-size: 30px
+            border-radius: 25px;
+            border: 1px solid black;
+            cursor: pointer;
+            display: inline-block;
+            text-decoration: none;
+            background-color: lime;
+            outline: 0;
+          }
+          button:focus {
+            outline:0;
+          }
+          button:active {
+            outline: 0;
+            background-color: yellow;
+          }
+          .fortune-box {
+            font-size: 36px;
+            padding: 15px;
+          }
+          .wrapper {
+            text-align: center;
+          }
+        `}
+      </style>
+    </div>
+  )
 }
 
 export default Index
